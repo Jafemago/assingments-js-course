82d739612576c4551f52dbc1f06fb2b33aa898c4
Javier Mateus <151576856+Jafemago@users.noreply.github.com>
2023-11-23 16:23:45 -0400

    Create introduction-to-objects.js

---
+++ introduction-to-objects.js
@@ -0,0 +1,10 @@
+// LECTURE: Introduction to Objects
+// 1. Create an object called 'myCountry' for a country of your choice, containing properties 'country', 'capital', 'language', 'population' and 'neighbours' (an array like we used in previous assignments)
+
+const myCountry = {
+    country: "Colombia",
+    capital: "Bogota",
+    language: "Spanish",
+    population: "50 million",
+    neighbours: ["Brazil", "Panama", "Venezuela", "Ecuador", "Peru"]
+}
