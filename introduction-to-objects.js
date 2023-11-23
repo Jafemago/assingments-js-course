9d89a260e11d42221bf3326f4863a651b1cfc15c
Javier Mateus <151576856+Jafemago@users.noreply.github.com>
2023-11-23 17:05:55 -0400

    Update introduction-to-objects.js

--- introduction-to-objects.js
+++ introduction-to-objects.js
@@ -8,3 +8,12 @@ const myCountry = {
     population: "50 million",
     neighbours: ["Brazil", "Panama", "Venezuela", "Ecuador", "Peru"]
 }
+
+// LECTURE: Dot vs. Bracket Notation
+// 1. Using the object from the previous assignment, log a string like this to the console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries and a capital called Helsinki.'
+// 2. Increase the country's population by two million using dot notation, and then decrease it by two million using brackets notation.
+
+console.log(`${myCountry.country} has ${myCountry.population} ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`);
+
+myCountry.population = "52 million";
+myCountry["population"] = "50 million";
